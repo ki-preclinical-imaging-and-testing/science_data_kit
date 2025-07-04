7fd4a5a9335c7f868a426e9a3276a27846b5cc73
Adam Patch <ae.patch@gmail.com>
2025-07-04 02:43:07 -0400

    Complete verification of remaining utilities and update roadmap

--- app/utils/registry.py
+++ app/utils/registry.py
@@ -1,3 +1,18 @@
+"""
+This module is deprecated and has been moved to science_data_kit/core/utils/registry_utils.py.
+Please update your imports to use the new location.
+
+Example:
+    from science_data_kit.core.utils.registry_utils import get_registered_model, register_model
+"""
+
+# Raise an ImportError to ensure that any code still using this module will fail
+# and be updated to use the new location
+raise ImportError(
+    "This module has been moved to science_data_kit/core/utils/registry_utils.py. "
+    "Please update your imports to use the new location."
+)
+
 from neomodel import (
     StructuredNode, StringProperty, UniqueIdProperty,
     RelationshipTo,
